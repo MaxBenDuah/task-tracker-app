b814650ff4c96bc85e7fc2fb5ea551eda938bbf0
MaxBenDuah <maxwellben728@gmail.com>
2024-11-09 07:11:05 +0000

    Adjust TaskList component to filter search based on status either completed or pending

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -10,7 +10,8 @@ function TaskList() {
       : tasks.filter(
           (task) =>
             task.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            task.description.toLowerCase().includes(searchTerm.toLowerCase())
+            task.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            task.completed.toLowerCase().includes(searchTerm.toLowerCase())
         );
 
   return (
